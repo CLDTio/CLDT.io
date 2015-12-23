ba44c39c11f8cc107ad7957e4312e69f8fb94f77
josmas <josmasflores@gmail.com>
2015-12-23 21:11:01 +0000

    adds console logging to the server

--- server_services/operational_api/index.js
+++ server_services/operational_api/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Good = require('good');
 
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
@@ -19,6 +20,27 @@ server.route({
   handler: function (request, reply) {
     reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
   }
+});
+
+server.register({
+  register: Good,
+  options: {
+    reporters: [{
+      reporter: require('good-console'),
+      events: {
+        response: '*',
+        log: '*'
+      }
+    }]
+  }
+}, (err) => {
+  if (err) {
+    throw err; // something bad happened loading the plugin
+  }
+
+  server.start(() => {
+    server.log('info', 'Server running at: ' + server.info.uri);
+  });
 });
 
 server.start(() => {
