af7ba75f507a51da615a764c3628a93d216c8157
josmas <josmasflores@gmail.com>
2016-01-05 14:19:23 +0000

    tidy up script

--- server_tools/docker_node-red/runNodeRed.sh
+++ server_tools/docker_node-red/runNodeRed.sh
@@ -1,1 +1,5 @@
-docker run -it --rm -p 1880:1880 -v /home/jos/.node-red:/home/jos/.node-red josmas/node-red
+docker run -it --rm \
+  --name nodered \
+  -p 1880:1880 \
+  -v /home/jos/.node-red/:/home/jos/.node-red/ \
+  josmas/node-red
