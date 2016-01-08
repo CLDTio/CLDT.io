984cd70a909212e3704dd558330184778b87db4c
josmas <josmasflores@gmail.com>
2016-01-08 18:04:47 -0100

    adds contrib source folder as a volume

--- server_tools/docker_node-red/runMacNodeRed.sh
+++ server_tools/docker_node-red/runMacNodeRed.sh
@@ -1,4 +1,5 @@
 docker run -it --rm \
   -p 1880:1880 \
   -v /Users/jos/.node-red:/home/jos/.node-red \
+  -v /Users/jos/development/nodejs/node-red-contrib:/home/jos/node-red-contrib \
   josmas/node-red
