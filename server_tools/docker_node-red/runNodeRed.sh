4abf3cd7bf99734725186e903e86862ded930bb3
josmas <josmasflores@gmail.com>
2016-01-11 12:48:41 +0000

    adds contrib folder to script for node-red nodes development

--- server_tools/docker_node-red/runNodeRed.sh
+++ server_tools/docker_node-red/runNodeRed.sh
@@ -2,4 +2,5 @@ docker run -it --rm \
   --name nodered \
   -p 1880:1880 \
   -v /home/jos/.node-red/:/home/jos/.node-red/ \
-  josmas/node-red
+  -v /home/jos/development/nodejs/node-red-contrib:/home/jos/node-red-contrib \
+  josmas/node-red /bin/bash
