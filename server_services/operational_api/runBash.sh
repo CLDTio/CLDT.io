5e9c61c683b0afdbb3c965c9f786d41e4ca26f13
josmas <josmasflores@gmail.com>
2016-01-14 15:58:15 +0000

    bespoke container for operational api

--- server_services/operational_api/runBash.sh
+++ server_services/operational_api/runBash.sh
@@ -1,5 +1,5 @@
 docker run -it --rm \
-  --name operational_api \
+  --name op-api \
   -p 3000:3000 \
   -v /home/jos/development/Insight/CLDT.io/server_services/operational_api:/home/jos/operational_api \
-  node:5.1.0 /bin/bash
+  josmas/op-api
