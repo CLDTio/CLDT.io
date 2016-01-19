7a4c3ce5d1ef8edb8f931a33b64db10fc4b009e2
josmas <josmasflores@gmail.com>
2016-01-19 16:32:36 +0000

    sharing nodes development folder with container

--- server_tools/docker_node-red/runMacNodeRed.sh
+++ server_tools/docker_node-red/runMacNodeRed.sh
@@ -15,4 +15,5 @@ colordocker run -it --rm \
   -p 1880:1880 \
   -v /Users/jos/.node-red:/home/jos/.node-red \
   -v /Users/jos/development/nodejs/node-red-contrib:/home/jos/node-red-contrib \
+  -v /Users/jos/development/Insight/CLDT.io/ui_services/cldt-ui-nodes:/home/jos/cldt-ui-nodes \
   josmas/node-red /bin/bash
